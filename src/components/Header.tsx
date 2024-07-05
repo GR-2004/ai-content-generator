ebaed208ec837ea7b4b764864e72fb82e70507b3
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-07-05 17:12:26 +0530

    add sidebar and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Search } from 'lucide-react'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='p-5 shadow-sm border-b-2 flex justify-between items-center'>
+      <div className='flex gap-2 items-center p-2 border rounded-md max-w-lg'>
+      <Search/>
+      <input type="text" placeholder='Search...' className='outline-none' />
+      </div>
+      <div>
+        <h2 className='bg-primary p-1 rounded-lg text-xs text-white px-2'>Join Membership just for $9.99/month</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,65 @@
+'use client'
+import Image from "next/image";
+import React, { useEffect } from "react";
+import {
+  FileClock,
+  Home,
+  LayoutDashboard,
+  Settings,
+  WalletCards,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const SideNav = () => {
+
+  const MenuList = [
+    {
+      name: "Home",
+      icon: Home,
+      path: "/",
+    },
+    {
+      name: "Dashboard",
+      icon: LayoutDashboard,
+      path: "/dashboard",
+    },
+    {
+      name: "History",
+      icon: FileClock,
+      path: "/history",
+    },
+    {
+      name: "Billing",
+      icon: WalletCards,
+      path: "/billing",
+    },
+    {
+      name: "Setting",
+      icon: Settings,
+      path: "/setting",
+    },
+  ];
+
+  const path = usePathname();
+  useEffect(() => {
+
+  }, [])
+  return (
+    <div className="h-screen p-5 shadow-sm border ">
+      <div className="flex justify-center">
+        <Image src={"/logo.svg"} alt="logo" width={120} height={100} />
+      </div>
+      <hr className="my-6 border" />
+      <div className="mt-3">
+        {MenuList.map((menu, index) => (
+          <div key={index} className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${path == menu.path && "bg-primary text-white"}`}>
+            <menu.icon className="h-6 w-6" />
+            <h2 className="text-lg">{menu.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
