6a57abde3897f5643307bc548305169adc43325d
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-07-06 17:19:00 +0530

    added text editor using toast UI

--- src/app/dashboard/content/_components/OutputSection.tsx
+++ src/app/dashboard/content/_components/OutputSection.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 
 const OutputSection = () => {
+  const editorRef:any = useRef();
   return (
-    <div>
-      
+    <div className='bg-white shadow-lg border rounded-lg'>
+      <div className='flex justify-between items-center p-5'>
+        <h2 className='font-medium text-lg'>Your Result</h2>
+        <Button className='flex gap-2'><Copy className='w-4 h-4'/>Copy</Button>
+      </div>
+       <Editor
+       ref={editorRef}
+    initialValue="Your result will appear here"
+    height="600px"
+    initialEditType="wysiwyg"
+    useCommandShortcut={true}
+    onChange={() => console.log(editorRef.current.getInstance().getMarkdown())}
+  />
     </div>
   )
 }
+
 
 export default OutputSection
