e9f4ab45feb76299a599ad52aea62311431f7636
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-07-27 16:36:20 +0530

    add mongoose and userSubscription and Aioutput models

---
+++ src/models/AIOutput.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+// Define the interface for the AIOutput document
+export interface AIOutputInterface extends Document {
+    formData: string;
+    aiResponse?: string; // Marking aiResponse as optional
+    templateSlug: string;
+    createdBy: string;
+    createdAt: Date;
+}
+
+// Define the schema for AIOutput
+const AIOutputSchema: Schema<AIOutputInterface> = new Schema({
+    formData: {
+        type: String,
+        required: [true, "formData is required"],
+    },
+    aiResponse: {
+        type: String,
+    },
+    templateSlug: {
+        type: String,
+        required: [true, "templateSlug is required"],
+    },
+    createdBy: {
+        type: String,
+        required: [true, "createdBy is required"],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, // Use Date.now without parentheses because when you are using parentheses it will store first Date value, not current value
+    },
+});
+
+// Create the model for AIOutput
+const AIOutput =
+    (mongoose.models.AIOutput as mongoose.Model<AIOutputInterface>) ||
+    mongoose.model<AIOutputInterface>("AIOutput", AIOutputSchema);
+
+export default AIOutput;
---
+++ src/models/UserSubscription.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface UserSubscriptionInterface extends Document {
+    username: string;
+    email: string;
+    active: boolean;
+    paymentId: string;
+    joinDate: Date;
+}
+
+const UserSubscriptionSchema: Schema<UserSubscriptionInterface> = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+\@.+\..+/, "please use a valid email address"],
+    },
+    active: {
+        type: Boolean,
+        default: false,
+    },
+    paymentId: {
+        type: String,
+    },
+    joinDate: {
+        type: Date,
+    }
+})
+
+const UserSubscription = (mongoose.models.UserSubscription as mongoose.Model<UserSubscriptionInterface>) || mongoose.model<UserSubscriptionInterface>("UserSubscription", UserSubscriptionSchema);
+
+export default UserSubscription
